b73808c794284cec222d5b686331d1d17d33db03
Mxn44n <149548609+Mxn44n@users.noreply.github.com>
2025-07-24 21:17:55 +0100

    finished week 4

--- life/life.py
+++ life/life.py
@@ -1,14 +1,42 @@
+"""An implementation of Conway's game of life."""
 import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
+from math import floor, ceil
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+
+class Pattern:
+    """Controls movement of pattern."""
+
+    def __init__(self, grid):
+        self.grid = grid
+
+    def flip_vertical(self):
+        """Vertical reflection."""
+        return Pattern(self.grid[::-1, :])
+
+    def flip_horizontal(self):
+        """Horizontal reflection."""
+        return Pattern(self.grid[:, ::-1])
+
+    def flip_diag(self):
+        """Diagonal reflection."""
+        return Pattern(self.grid.T)
+
+    def rotate(self, n):
+        """Rotation anti-clockwise in right-angle multiples."""
+        out = self
+        for i in range(n):
+            out = out.flip_diag().flip_vertical()
+        return out
+
+
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
-    )
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -49,30 +77,45 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    """A game of Conway's Life with a square board of side length size."""
+
+    def __init__(self, size):
+        self.board = np.zeros((size, size))
+
     def play(self):
+        """Play the game indefinitely until interrupted."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        """Play one step of the game."""
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (neighbourcount[i, j] == 3 or (
+                    neighbourcount[i, j] == 2 and self.board[i, j])) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """Insert a pattern centred on the location given."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Visualise the current game board."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
+
+    def insert(self, pattern, location):
+        """Put pattern on map."""
+        rows, cols = pattern.grid.shape
+        x, y = location
+        self[x - floor(rows/2):x + ceil(rows/2),
+             y - floor(cols/2):y + ceil(cols/2)] = pattern.grid
